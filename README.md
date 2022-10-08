723a034227728797e7ce04ee9cc1ac1388c75a47
osbm <osmanfbayram@gmail.com>
2022-10-08 17:42:17 +0300

    add descriptions

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # CMP2003
 
+
+- Week 1
+    - **exercise_1**: hello world
+    - **exercise_2**: random number generation and assert keyword
---
+++ week_1/exercise_2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cassert>
+#include <time.h>
+
+using namespace std;
+
+int main() {
+    // to get different random number everytime
+    srand(time(NULL));
+
+    // generate random number between 0 and 1
+    double random_number = (double) rand() / RAND_MAX;
+
+
+    cout << "Random number: " << random_number << endl;
+    assert (random_number < 0.5);    
+    return 0;
+}
